c8207dab66c2fffc88dbeae4dadd12a38713eb2f
Cole Cota <cjcota@ucsc.edu>
2020-04-19 17:37:19 -0700

    N

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -14,7 +14,7 @@ class Menu extends Phaser.Scene {
 
     create() {
         //menu display
-        this.add.image(640, 480, 'starfield').setOrigin(50,50);
+        this.add.image(640, 480, 'starfield').setOrigin(1);
 
         let menuConfig = {
             fontFamily: 'Courier',
